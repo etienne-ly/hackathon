9882dbaa888df38b41fa9ae98e1fd7ad3bae4ac8
Orlando <dieo6736@usherbrooke.ca>
2025-11-01 16:42:04 -0400

    news images

--- src/app/service/imgApi.service.ts
+++ src/app/service/imgApi.service.ts
@@ -8,21 +8,26 @@ import {firstValueFrom} from 'rxjs';
 })
 export class ImgApiService {
 
+  private sleep(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
   async getImage(query: string, rank: number = 0): Promise<string> {
-    const url = `https://corsproxy.io/?https://search.brave.com/images?q=${encodeURIComponent(query)}`;
+    const url = `https://corsproxy.io/?https://www.ecosia.org/images?q=${encodeURIComponent(query)}`;
 
     const response = await fetch(url);
     const html = await response.text();
 
     const parser = new DOMParser();
     const doc = parser.parseFromString(html, 'text/html');
-    const images = Array.from(doc.querySelectorAll('img[data-rank]'));
+    const images = Array.from(doc.querySelectorAll('img'));
 
     const rankedImages: string[] = images
       .map(img => {
         return img.getAttribute('src') || img.getAttribute('data-src') || '';
       })
 
+    await this.sleep(100);
     return rankedImages[rank];
   }
 
