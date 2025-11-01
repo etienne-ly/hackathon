0b570815c181d4245993cae3e62a198bf2aaf295
etienne ly <etiennely@hotmail.com>
2025-11-01 16:31:15 -0400

    new api and basic news template

--- src/app/templates/news-template/news-template.ts
+++ src/app/templates/news-template/news-template.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ApiService} from '../../../service/api-service';
 
 interface PageContent {
+  color: string;
   title: string;
   highlightTitle: string;
   highlightDescription: string;
@@ -20,12 +21,13 @@ export class NewsTemplate implements OnInit {
 
   async ngOnInit() {
     console.log("Sending request to api");
-    this.pageContent = await this.api.get<PageContent>("Generate me, in raw JSON format, a webpage content filling this criteria: '{\n" +
-      "  title: string;\n" +
-      "  highlightTitle: string;\n" +
-      "  highlightDescription: string;\n" +
-      "  highlightCategory: string;\n" +
-      "  latest: { title: string; description: string; category: string }[]\n" +
+    this.pageContent = await this.api.get<PageContent>("Generate me a webpage content FILLING this format ONLY (no extra words or characters): '{\n" +
+      "  \"color\"; string;\n" +
+      "  \"title\": string;\n" +
+      "  \"highlightTitle\": string;\n" +
+      "  \"highlightDescription\": string;\n" +
+      "  \"highlightCategory\": string;\n" +
+      "  \"latest\": { \"title\": string; \"description\": string; \"category\": string }[9]\n" +
       "}', based on the following title: 'Buggle' and the following url: 'www.buggle.com'");
   }
 
