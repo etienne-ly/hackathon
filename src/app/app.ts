02e2cb714f0241b41e43a8a6f749c717983baa97
Andrew <loza3285@usherbrooke.ca>
2025-11-01 11:57:31 -0400

    Feat: outlook

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MailComponent} from './mail-page/mail-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MailComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
