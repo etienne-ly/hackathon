543ab9c8553d65c46a4f0ce092febd6355445e2e
etienne ly <etiennely@hotmail.com>
2025-11-01 14:45:48 -0400

    ai api and browser content background

--- src/app/browser/browser.ts
+++ src/app/browser/browser.ts
@@ -2,11 +2,14 @@ import {Component, Input} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {Tab} from '../../models/state';
 import {GameService} from '../../service/game.service';
+import {PortfolioTemplate} from '../templates/portfolio-template/portfolio-template';
+import {ApiService} from '../../service/api-service';
 
 @Component({
   selector: 'app-browser',
   imports: [
     RouterOutlet,
+    PortfolioTemplate,
   ],
   templateUrl: './browser.html',
   styleUrl: './browser.css',
