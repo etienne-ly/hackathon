0110e3438fbb71debf1eb6c9b4c5d6c9a7dd9503
Andrew <loza3285@usherbrooke.ca>
2025-11-01 17:32:05 -0400

    feat : browser behavior

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {MailComponent} from './mail-page/mail-page.component';
+import {DormComponent} from '../dorm/dorm';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+  path: 'insideMail',
+  component: MailComponent
+}, {
+  path: 'dorm',
+  component: DormComponent
+},
+];
