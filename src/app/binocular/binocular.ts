c29f4efb2ca3b03584db0aaf5a9e9dcf4f335ea2
Andrew <loza3285@usherbrooke.ca>
2025-11-02 03:06:44 -0500

    feat: loading screen in each page

--- src/app/binocular/binocular.ts
+++ src/app/binocular/binocular.ts
@@ -6,17 +6,22 @@ import {NewsTemplate} from '../templates/news-template/news-template';
 import {ForumTemplate} from '../templates/forum-template/forum-template';
 import {ECommerceTemplate} from '../templates/e-commerce.template/e-commerce.template';
 import {BusinessTemplate} from '../templates/business/business.template';
+import {BinocularSkeleton} from './binocular-skeleton/binocular.skeleton';
 
 @Component({
   selector: 'app-binocular',
   templateUrl: './binocular.html',
   styleUrl: './binocular.css',
+  imports: [
+    BinocularSkeleton
+  ]
 })
 export class Binocular implements OnInit {
 
   @Input() replaceTab: Function = () => alert('hell nah');
   @Input() search: string = "";
   results: WebPage[] = [];
+  loading: boolean = false;
 
   constructor(public game: GameService, private aiService: AiService) {
   }
@@ -25,8 +30,10 @@ export class Binocular implements OnInit {
     if (this.search == "") {
       return;
     }
+    this.loading = true;
 
     this.results = await this.aiService.get<WebPage[]>(`Generate me fake webpage content FILLING this JSON format ONLY (no extra words or characters): { "template": "news" | "forum" | "e-commerce" | "business", "domain": string, "title": string, "description": string, "url": string }[12], based on the following search string: "${this.search}".`)
+    this.loading = false;
   }
 
   getComponentFromString(value: 'news' | 'forum' | 'e-commerce' | 'business'): Type<any> {
